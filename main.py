c7a72203a08c138ec1ddc547c06fe050bb670066
Mikhail Mamontov <mmontovmm@gmail.com>
2025-02-08 23:57:09 +1000

    create bot

---
+++ main.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+import asyncio
+
+bot = Bot('8101193115:AAFwZ6wJTlrvolPdceh0paNL1UP2NazCMlw')
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def process_start(message: Message):
+    await message.answer(text='Привет! Это обычный бот.')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
