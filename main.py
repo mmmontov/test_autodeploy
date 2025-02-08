933d9b81409c6fa22b33a3fe4324cd270a11c95a
Mikhail Mamontov <mmontovmm@gmail.com>
2025-02-09 01:49:47 +1000

    chenge

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def process_start(message: Message):
-    await message.answer(text='Привет! Это автобот в разработке изменённый замерженый.')
+    await message.answer(text='Привет! Это автобот.')
 
 
 async def main():
